618f26ba9c7a42d66edb7c2c6be64d7b2a1d3ad3
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-09 17:46:28 -0300

    Add files via upload

---
+++ DML/current_queries.sql
@@ -0,0 +1,23 @@
+﻿SELECT backend_start as be_start,
+       datname,
+       pid as pid,
+       client_addr,
+       usename as user,
+       state,
+       query,
+       wait_event_type,          --< COMMENT OUT FOR 9.4 and below
+/*                               --< UNCOMMENT FOR 9.4 and below
+       CASE WHEN waiting = TRUE  
+            THEN 'BLOCKED'
+            ELSE 'no'
+        END as waiting,
+*/        
+       query_start,
+       current_timestamp - query_start as duration 
+  FROM pg_stat_activity
+ WHERE pg_backend_pid() <> pid
+ORDER BY 1, 
+         datname,
+         query_start;
+
+--SELECT * FROM pg_stat_activity LIMIT 2;
---
+++ DML/triggers.sql
@@ -0,0 +1,19 @@
+﻿SELECT n.nspname AS schema,
+       c.relname AS table,
+       t.tgname  AS trigger, 
+       p.proname AS function_called,
+       CASE WHEN t.tgconstrrelid > 0
+            THEN (SELECT relname 
+                   FROM pg_class 
+                  WHERE oid = t.tgconstrrelid)
+            ELSE ''
+        END      AS constr_tbl,
+       t.tgenabled AS mode,
+       t.tgconstrindid
+  FROM pg_trigger t
+  INNER JOIN pg_proc p  ON ( p.oid = t.tgfoid)
+  INNER JOIN pg_class c ON (c.oid = t.tgrelid)
+  INNER JOIN pg_namespace n ON (n.oid = c.relnamespace)
+  WHERE tgname NOT LIKE 'pg_%' 
+    AND tgname NOT LIKE 'RI_%'  -- < comment out to see triggers
+ ORDER BY 1, 2;

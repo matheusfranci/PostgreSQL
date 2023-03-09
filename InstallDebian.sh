255eabe61f9f1bb8253bc86629e1a8c42cc0463c
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2023-03-08 23:33:30 -0300

    Delete InstallDebian.sh

--- InstallDebian.sh
+++
@@ -1,7 +0,0 @@
-$ sudo apt update && sudo apt upgrade
-
-$ sudo apt install postgresql postgresql-contrib
-
-root@MatheusHO:/etc#  sudo systemctl status PostgreSQL
-System has not been booted with systemd as init system (PID 1). Can't operate.
-Failed to connect to bus: Host is down

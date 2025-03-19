6c1dd3be9b406628eddbfb86fbdedc4a58247e7b
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2025-03-19 15:10:14 -0300

    Delete DDL/CreateTablespace.sql

--- DDL/CreateTablespace.sql
+++
@@ -1,5 +0,0 @@
--- Com owner que estiver logado
-CREATE TABLESPACE ts_primary LOCATION 'c:\pgdata\primary';
-
--- Com outro owner
-CREATE TABLESPACE indexspace OWNER genevieve LOCATION '/data/indexes';

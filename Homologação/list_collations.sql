85581a2196dd75c1713696ea5f0746bdb5b8d872
Matheus Francisco <67348204+matheusfranci@users.noreply.github.com>
2025-03-24 17:40:20 -0300

    Update and rename list_collations.sql to collations.md

--- Homologação/list_collations.sql
+++
@@ -1,3 +0,0 @@
--- https://www.cybertec-postgresql.com/en/case-insensitive-pattern-matching-in-postgresql/
--- https://database.guide/how-to-return-a-list-of-available-collations-in-postgresql/
-SELECT * FROM pg_collation WHERE collname like '%en_US%';
